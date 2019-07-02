d338de6ef8da3b58c3def028f2e1b5a58c5ca4ea
TIS2o1 <tasnim.tis@gmail.com>
2019-07-02 06:34:33 +0600

    new commit

--- src/login/CompanyHomeController.java
+++ src/login/CompanyHomeController.java
@@ -5,10 +5,22 @@
  */
 package login;
 
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -25,10 +37,45 @@ public class CompanyHomeController implements Initializable {
         // TODO
     }   
     
+    private Company comp;
+    @FXML private Label CompanyName;
     
-    private String eml;
-    public CompanyHomeController(String eml){
-        this.eml = eml;
+    public void showAddCarView(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("AddCar.fxml"));
+        Parent parent = loader.load();
+        
+        Scene scene = new Scene(parent);
+        
+        //AddCarController controller = loader.getController();
+        //controller.companyInformation(comp);
+        
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setTitle("Add Car");
+        window.setScene(scene);
+        window.show();
     }
     
+    public void showViewCars(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("CarView.fxml"));
+        Parent parent = loader.load();
+        
+        Scene scene = new Scene(parent);
+        
+        CarViewController controller = loader.getController();
+        controller.createTable(comp);
+        
+        Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        
+        window.setTitle("View all Cars");
+        window.setScene(scene);
+        window.show();
+    }
+    
+    public void initData(Company comp){
+        this.comp = comp;
+        CompanyName.setText((this.comp).getName());
+    }
 }
