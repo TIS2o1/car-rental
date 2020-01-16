82b50982410a57f8d534c8e4f73af289688f386c
TIS2o1 <tasnim.tis@gmail.com>
2020-01-16 12:02:52 +0600

    final

--- src/login/Car.java
+++ src/login/Car.java
@@ -7,22 +7,45 @@ package login;
 
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.image.ImageView;
+//import javafx.scene.image.Image;
+
 
 
 public class Car {
-    private SimpleStringProperty id,model,availability;
+    private SimpleStringProperty id,model,availability,compName,location;
     private SimpleIntegerProperty seat;
     private SimpleDoubleProperty price;
+    private ImageView image;
     
-    public Car(String id, String model, String availab, int seat, double price){
+    public Car(){
+    }
+    
+    public Car(String id, String model, String availability, int seat, double price, ImageView image){
         this.id = new SimpleStringProperty(id);
         this.model = new SimpleStringProperty(model);
-        this.availability = new SimpleStringProperty(availab);
+        this.availability = new SimpleStringProperty(availability);
         this.seat = new SimpleIntegerProperty(seat);
         this.price = new SimpleDoubleProperty(price);
+        this.image = image;
         
     }
+    
+    public Car(String id, String model, String availability, int seat, double price, String compName,String location,ImageView image){
+        this.id = new SimpleStringProperty(id);
+        this.model = new SimpleStringProperty(model);
+        this.availability = new SimpleStringProperty(availability);
+        this.seat = new SimpleIntegerProperty(seat);
+        this.price = new SimpleDoubleProperty(price);
+        this.compName = new SimpleStringProperty(compName);
+        this.location = new SimpleStringProperty(location);
+        this.image = image;
+        
+    }
+    
+    
 
     public String getId() {
         return id.get();
@@ -64,8 +87,24 @@ public class Car {
         this.price = new SimpleDoubleProperty(price);
     }
 
+    public void setCompName(String compName) {
+        this.compName = new SimpleStringProperty(compName);
+    }
+
+    public String getCompName() {
+        return compName.get();
+    }
     
-    
-    
+    public String getLocation() {
+        return location.get();
+    }
+
+    public ImageView getImage() {
+        return image;
+    }
+
+    public void setImage(ImageView image) {
+        this.image = image;
+    }
     
 }
